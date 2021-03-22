19df158cc3d759e1565fb2036c8e976b173a8a6f
greenichi <rina.tanaka@di-v.co.jp>
2021-03-22 15:43:51 +0900

    追加

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -6,6 +6,9 @@ FactoryBot.define do
 
     after(:build) do |message|
       message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      # io: File.openで設定したパスのファイル（public/images/test_image.png）を、test_image.pngというファイル名で保存をしています。
+      # after(:build)だとテキストと画像が同時に保存され、after(:create)だとテキストは別で画像のみ先に保存されてしまう
+      # 独立して保存されるのは今回望ましくない
     end
   end
 end
